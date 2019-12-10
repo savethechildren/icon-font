b936e298e44d8536ab6b31e4518af30b1104d445
Damien North <damien.north@savethechildren.org>
2019-12-10 13:46:14 +0000

    Upgrade to FontAwesome 5.11.2

--- gulpfile.js
+++ gulpfile.js
@@ -13,36 +13,12 @@ var dest = {
 
 
 var gulp = require("gulp"),
-  svg2png = require('gulp-svg2png-update'),
   sass = require('gulp-sass'),
   concat = require('gulp-concat'),
   uglify = require('gulp-uglify'),
   rename = require('gulp-rename'),
   watch = require('gulp-watch'),
   each = require('gulp-each');
- 
-gulp.task('svg2png', async function () {
-    gulp.src(src.svg + '**/*.svg')
-        .pipe(svg2png({
-            width:192,
-            height:192
-        }))
-        .pipe(gulp.dest(dest.png + '/192x192'));
-
-    gulp.src(src.svg + '**/*.svg')
-        .pipe(svg2png({
-            width:96,
-            height:96
-        }))
-        .pipe(gulp.dest(dest.png + '/96x96'));
-        
-    gulp.src(src.svg + '**/*.svg')
-        .pipe(svg2png({
-            width:48,
-            height:48
-        }))
-        .pipe(gulp.dest(dest.png + '/48x48'));
-});
 
 gulp.task('sass', function () {
     return gulp.src(src.scss + 'stc-icons.scss')
